c34935e5e3d8f6b8e34c1c0ed756b85cc4c78d92
Alexandr Prudnikov <prudnikov20@yandex.ru>
2023-08-24 16:18:05 +0300

    add rateLimit

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Слишком много запросов, пожалуйста, повторите попытку позже',
+});
 
 const app = express();
 
@@ -16,6 +22,7 @@ mongoose.connect(DB_URL, {});
 
 app.use(cors());
 app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(requestLogger);
 
